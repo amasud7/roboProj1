fcc53ac25aa2d93f9c726d85bb02786c6a384277
Ayad Masud <ayad.r.masud@gmail.com>
2024-11-07 18:29:07 -0600

    its working pretty well for 1, 2, 3.. 4 and 5 yet to be determined

--- compute_moments.py
+++ compute_moments.py
@@ -5,9 +5,10 @@ import os
 
 def get_contour(image): # process image and return major contour aka number
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    _, thresh = cv2.threshold(gray, 65, 255, cv2.THRESH_BINARY) 
+    blurred = cv2.GaussianBlur(gray, (7, 7), 0)
+    _, thresh = cv2.threshold(blurred, 100, 255, cv2.THRESH_BINARY) 
 
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE) # change to not simple for more points
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # change to NONE for more points
 
     child_contours = [] 
     for i, h in enumerate(hierarchy[0]): 
@@ -34,21 +35,25 @@ def get_contour(image): # process image and return major contour aka number
 
 def euclidian_distance(contour): # computes euclidian distance between contour and template
     # store hu moments of all templates somehow --> currently testing with this
-    # temp_hu_moments = [-0.42033896, -1.2800598, -2.37100151, -3.00287561, -5.72290071, -3.64337416, -6.11469886] # this is for standard_3
-    temp_hu_moments =[-0.3762888, -1.06797808, -2.28293167, -3.00242794, -5.65180683, -3.53765858, -6.40381786] # standard_3 resized to 640x640
-    # temp_hu_moments = [-0.41209761, -1.0100353,  -1.99089754, -2.54051226, -4.84663678, -3.0727363, -5.1911946 ] # hero_1
+    # temp_hu_moments = [0.38479041, 1.08707333, 2.3122054,  3.03535197, -5.71430297, -3.58084251, -6.52324638] # standard_3 resized to 640x640
+    # temp_hu_moments = [0.20190165, 0.46352852, 1.51366024, 1.6986363, 3.30562525, 1.9312427, -4.51126758] # hero_1 resized to 640x640
+    # temp_hu_moments = [0.40817217, 1.20667722, 3.41604175, 4.11077181, 8.02564175, -5.41968741, -8.02374956] # standard_5 resized to 640x640
+    temp_hu_moments =  [0.37610695, 1.11190971, 2.66961032, 3.30066991, 6.57430505, 4.32652557, 6.35262385] # engineer_2 resized to 640x640
 
     moments = cv2.moments(contour)
     img_hu_moments = cv2.HuMoments(moments).flatten()
 
     # proper log transform?
+    for i in range(len(img_hu_moments)):
+        img_hu_moments[i] = -1 * np.copysign(1.0, img_hu_moments[i]) * np.log10(abs(img_hu_moments[i]))
     
 
     # euclidian distance computation
     difference = np.sum(np.abs(-np.sign(temp_hu_moments) * np.log10(np.abs(temp_hu_moments)) - 
                                -np.sign(img_hu_moments) * np.log10(np.abs(img_hu_moments))))
+    distance = np.linalg.norm(temp_hu_moments - img_hu_moments)
 
-    return difference
+    return distance
 
 
 def main():
@@ -57,7 +62,7 @@ def main():
     difference_list = []
     difference_list2 = []
     image_num = 0
-    for image in os.scandir('/Users/amasud7/Desktop/code/roboProj1/test_1'):
+    for image in os.scandir('/Users/amasud7/Desktop/code/roboProj1/test_2'):
         if image.path.endswith('.jpg'):
             # for each image compute euclidean distance and push back to list
             
@@ -81,6 +86,8 @@ def main():
 # print(difference)
 
 main()
+# threshhold under 10 is looking pretty good
+# 5 does not seem to work well (15.00) --> use orb to distinguish between the two??
 
 
 
@@ -88,18 +95,18 @@ main()
 
 
 
-
-# testing hu moments with these lines of code
+# # testing hu moments with these lines of code
 
 
 # # preprocess image
-# image = cv2.imread('images/standard_3.jpg')
+# image = cv2.imread('./images/engineer_2.jpg')
 
 # # change size to be the same as samples (640x640)
 # resize_img = cv2.resize(image, (640, 640))
 
 # gray = cv2.cvtColor(resize_img, cv2.COLOR_BGR2GRAY)
-# _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
+# blurred = cv2.GaussianBlur(gray, (7, 7), 0)
+# _, thresh = cv2.threshold(blurred, 100, 255, cv2.THRESH_BINARY)
 
 # # inner contour of image
 
@@ -131,19 +138,12 @@ main()
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
 
-# log = np.log10(np.abs(hu_moments))
-# print(log)
-# # template = [0.42033896, 1.2800598, 2.37100151, 3.00287561, -5.72290071, -3.64337416, -6.11469886] # standard_3
-# # template = [-0.3762888, -1.06797808, -2.28293167, -3.00242794, -5.65180683, -3.53765858, -6.40381786] # resized 640x640 standard_3
 
-# # template = [-0.41209761, -1.0100353,  -1.99089754, -2.54051226, -4.84663678, -3.0727363,
-# #  -5.1911946 ] # hero_1
+# # log transform
+# for i in range(len(hu_moments)):
+#     hu_moments[i] = -1 * np.copysign(1.0, hu_moments[i]) * np.log10(abs(hu_moments[i]))
 
 
-
-# # raw = [0.44113424, 0.9791763,  3.75028028, 3.88864883, 7.70819348, 4.38124476, 9.42470464] # (raw_3)
-
-
-# # # difference = np.sum(np.abs(-np.sign(template) * np.log10(np.abs(template)) - 
-# #                             #    -np.sign(raw) * np.log10(np.abs(raw))))
+# # log = np.log10(np.abs(hu_moments))
+# print(hu_moments)
 
